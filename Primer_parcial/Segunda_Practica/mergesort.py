bf967575feed03065dba6276f075e407da333a2f
boozinas <33131059+boozinas@users.noreply.github.com>
2018-03-05 19:53:53 -0600

    Codigo funcional arreglo con numero par de elementos

--- Primer_parcial/Segunda_Practica/mergesort.py
+++ Primer_parcial/Segunda_Practica/mergesort.py
@@ -5,18 +5,29 @@
 # Implementacion de algoritmo merge sort
 
 def mergesort(A):
-    print A
     n = len(A)
-    if n = 1:
+    if n == 1:
         return A
     Ap = mergesort(A[:n/2])
-    App = mergesort(A[n/2:])
-    return merge(Ap,App)
+    if n%2 != 1:
+        App = mergesort(A[n/2:])
+        return merge(Ap,App)
+    else:
+        App = mergesort(A[n/2:n-1])
+        res = []
+        res.append(A[-1])
+        return merge(merge(Ap,App),res)
 
 def merge(L,R):
-    R = []
-    
+    Re = []
+    for i in range(0,len(L)):
+        if L[i] < R[i]:
+            Re.append(L[i])
+            Re.append(R[i])
+        else:
+            Re.append(R[i])
+            Re.append(L[i])
+    return Re
 
-A = [13,23,1,232]
+A = [13,23,1,232,8]
 print A,mergesort(A)
-
