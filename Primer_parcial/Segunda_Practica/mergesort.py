929b70e415caab843f13fa16f16ffdef542b1218
alamRios <alam.rios@ymail.com>
2018-03-07 13:59:27 -0600

    Optimizacion merge sort

--- Primer_parcial/Segunda_Practica/mergesort.py
+++ Primer_parcial/Segunda_Practica/mergesort.py
@@ -14,20 +14,16 @@ def mergesort(A):
 
 def merge(L,R):
     Re = []
-    nL = len(L)
-    nR = len(R)
-    while nL > 0 and nR > 0:
-        if L[len(L) - nL] < R[len(R) - nR]:
-            Re.append(L[len(L) - nL])
-            nL -= 1
+    while len(L) > 0 and len(R) > 0:
+        if L[0] < R[0]:
+            Re.append(L.pop(0))
         else:
-            Re.append(R[len(R) - nR])
-            nR -= 1
-    if nL > 0:
-        Re += L[-nL:]
-    elif nR > 0:
-        Re += R[-nR:]
+            Re.append(R.pop(0))
+    if len(L) > 0:
+        Re += L
+    elif len(R) > 0:
+        Re += R
     return Re
 
-A = [1,0,-1,20,-20]
+A = [1,0,-1,20,1,-20,-50]
 print A,mergesort(A)
