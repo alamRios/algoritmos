6e8dd286f6c3c4a7fb316e8ae1222e4bdbe1d1ef
alamRios <alam.rios@ymail.com>
2018-03-13 09:08:34 -0600

    Ejercicio 3 fuerza bruta

---
+++ Primer_parcial/Tercer_practica/inversionesFB.py
@@ -0,0 +1,16 @@
+# inversionesFB.py
+# Medina Juarez Jesus Booz
+# Rios Altamirano Alam Yael
+# Algoritmo por fuerza bruta para encontrar inversiones
+# -> i < j -> A[i] > A[j]
+
+def inversiones(A):
+    indices = []
+    for i in range(0,len(A)-1):
+        for j in range(i+1, len(A)):
+            if(A[i] > A[j]):
+                indices.append((i,j))
+    return indices
+
+A = [2,4,1,3,5]
+print 'A:',A,'inversiones:',inversiones(A)
